422f9553ac84c91b02aa6005c6d35b1951b46e45
Angad Singh <angadkollege@gmail.com>
2025-04-15 05:01:50 +0530

    controller and schema changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   ],
   server: {
     proxy: {
-      "/api": {target:"https://singhstagram.onrender.com",
+      "/api": {target:"http://13.50.84.146:3000",
       changeOrigin: true,
       secure: false,}
     },
